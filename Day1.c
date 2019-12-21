e838a37bc117fccf0985c8d4804e5b5ff3b3e709
Yerra Naveen <55548341+Naveen-coder@users.noreply.github.com>
2019-12-21 23:25:11 +0530

    Add files via upload

---
+++ Day1.c
@@ -0,0 +1,10 @@
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main(int argc, char *a[])
+{
+char b[100];
+printf("Hello Techgig\n");
+scanf("%[^\n]",&b);
+printf("%s",b);
+	//Write code here
+}
